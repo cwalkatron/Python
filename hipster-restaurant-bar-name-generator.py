f14a6f897bf6080c3cfd8809702d0a39867c4022
Chris Walker <cwalker@pixar.com>
2013-12-12 11:24:47 -0800

    More words

--- hipster-restaurant-bar-name-generator.py
+++ hipster-restaurant-bar-name-generator.py
@@ -4,7 +4,8 @@ from random import choice
 
 words = "salt fat anchor longshoreman sail mustache saw beard churn weight " \
         "lamp wood whale axe pepper basil tamarind thyme cinnamon consumption " \
-        "burgandy"
+        "burgandy porch brew pickle candle toad spoon fork knife lamb " \
+        "oil kettle compass ox"
 
 words = words.split()
 
